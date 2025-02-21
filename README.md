2265d25efdff57bd2944f747f6e7c206de2e113c
davyli <dvli2007@gmail.com>
2025-02-20 19:41:19 -0500

    Commit code

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # hyperliquid-link-erc20
+
+## Setup
+
+1. Install python3
+
+2. Install requirements: `pip install -r requirements.txt`*
+  \**Hint: it's helpful to spin up a python virtual environment for installing the requirements. Run `python3 -m venv venv` to set up a virtual environment.*
+
+3. Set up your environment file: `cp env.example .env`
+
+4. Add your deployer wallet private key into the `.env` file.
+
+## Prerequisite
+
+1. Deploy your ERC20 contract address on HyperEVM
+
+2. Mint ERC20 tokens such that the token supply matches the spot token's total supply
+
+3. Send all ERC20 tokens to `0x2222222222222222222222222222222222222222`
+
+## Usage
+
+```
+python3 link_erc20.py <network> <erc20_contract_address> <token_index>
+```
+
+### Parameters
+
+**network** - the Hyperliquid network you wish to perform the linking on; must be either `mainnet` or `testnet`.
+
+**erc20_contract_address** - the address for your HyperEVM ERC20 contract
+
+**token_index** - the token index for the spot token you wish to link
+
+
+### Example
+
+```
+python3 link_erc20.py testnet 0x8cDE56336E289c028C8f7CF5c20283fF02272182 PURR 1
+```
+
+In the above example, we are linking PURR on Hyperliquid testnet to the ERC20 contract `0x8cDE56336E289c028C8f7CF5c20283fF02272182`.
---
+++ link_erc20.py
@@ -0,0 +1,109 @@
+import argparse
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+from eth_account import Account
+from eth_account.signers.local import LocalAccount
+from hyperliquid.info import Info
+from hyperliquid.utils import constants
+from hyperliquid.utils.signing import get_timestamp_ms, sign_l1_action
+from web3 import Web3
+from web3.middleware import SignAndSendRawMiddlewareBuilder
+
+
+MAINNET_EVM_RPC = 'https://rpc.hyperliquid.xyz/evm'
+TESTNET_EVM_RPC = 'https://rpc.hyperliquid-testnet.xyz/evm'
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('network', nargs=1)
+    parser.add_argument('contract_address', nargs=1)
+    parser.add_argument('token_index', nargs=1)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    network = args.network[0].lower()
+    contract_address = args.contract_address[0].lower()
+    token_index = int(args.token_index[0])
+
+    if network == 'mainnet':
+        L1_API_URL = constants.MAINNET_API_URL
+        EVM_RPC_URL = MAINNET_EVM_RPC
+    elif network == 'testnet':
+        L1_API_URL = constants.TESTNET_API_URL
+        EVM_RPC_URL = TESTNET_EVM_RPC
+    else:
+        raise Exception(f'Unrecognized network. Must be either "mainnet" or "testnet"')
+
+    # Connect to the JSON-RPC endpoint
+    w3 = Web3(Web3.HTTPProvider(EVM_RPC_URL))
+    info = Info(L1_API_URL, skip_ws=True)
+    try:
+        token_info = info.spot_meta()['tokens'][token_index]
+    except:
+        raise Exception(f'Token for index {token_index} not found')
+
+    # The account will be used for linking it to a native spot asset.
+    account: LocalAccount = Account.from_key(os.getenv('PRIVATE_ADDRESS'))
+    print(f'Running with address {account.address}')
+    w3.middleware_onion.inject(SignAndSendRawMiddlewareBuilder.build(account), layer=0)
+    w3.eth.default_account = account.address
+    # Verify connection
+    if not w3.is_connected():
+        raise Exception(f'Failed to connect to network')
+
+    # fetch information needed about erc20 token
+    erc20_contract = w3.eth.contract(
+        address=Web3.to_checksum_address(contract_address),
+        abi='[{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"}]',
+    )
+    erc20_decimals = erc20_contract.functions.decimals().call()
+    spot_token_name = token_info['name']
+    spot_decimals = token_info['weiDecimals']
+    extra_wei_decimals = max(erc20_decimals - spot_decimals, 0)
+    print(f'extra_wei_decimals={extra_wei_decimals}')
+
+    print()
+    print(f'You are about to link {spot_token_name} (index={token_index}) to ERC20 token {contract_address}.')
+    print('This action is irreversible.')
+    print()
+    user_confirmation = input('Proceed (y/n)? ').lower()
+    if user_confirmation != 'y':
+        raise Exception('Aborted by user')
+
+    print()
+    print('Proceeding with linking...')
+
+    action = {
+        "type": "spotDeploy",
+        "setEvmContract": {
+            "token": token_index,
+            "address": contract_address,
+            "evmExtraWeiDecimals": extra_wei_decimals,
+        },
+    }
+    nonce = get_timestamp_ms()
+    signature = sign_l1_action(account, action, None, nonce, False)
+    action["setEvmContract"]['address'] = contract_address
+    payload = {
+        "action": action,
+        "nonce": nonce,
+        "signature": signature,
+        "vaultAddress": None,
+    }
+    response = requests.post(L1_API_URL + "/exchange", json=payload)
+    print(response.json())
+
+
+if __name__ == '__main__':
+    load_dotenv('.env')
+
+    try:
+        main()
+    except Exception as e:
+        print(f'FAIL: {e}')
